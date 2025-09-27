85ec29befabd4c29a2f7b155be10834d81c8605d
toriium <John.Doe@hotmail.com>
2025-09-28 00:40:48 +0200

    feat: add reusable Ingredient

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 class Ingredient(ft.Column):
-    def __init__(self, name, fat, carbs, protein, kcal):
+    def __init__(self, name:str, fat:float = 0, carbs:float = 0, protein:float = 0, kcal:int = 0):
         super().__init__()
         self.name = name
         self.fat = float(fat)
@@ -10,17 +10,24 @@ class Ingredient(ft.Column):
         self.protein = float(protein)
         self.kcal = int(kcal)
 
-        self.txt_name = ft.TextField(label="Name", value=self.name, text_align=ft.TextAlign.LEFT)
-        self.txt_fat = ft.TextField(label="Fat", value=str(self.fat), text_align=ft.TextAlign.RIGHT)
-        self.txt_carbs = ft.TextField(label="Carbs", value=str(self.carbs), text_align=ft.TextAlign.RIGHT)
-        self.txt_protein = ft.TextField(label="Protein", value=str(self.protein), text_align=ft.TextAlign.RIGHT)
-        self.txt_kcal = ft.TextField(label="Kcal", value=str(self.kcal), text_align=ft.TextAlign.RIGHT)
+        self.txt_name = ft.TextField(label="Name", value=self.name, on_change=self.update_values, text_align=ft.TextAlign.LEFT)
+        self.txt_fat = ft.TextField(label="Fat", value=str(self.fat), on_change=self.update_values, text_align=ft.TextAlign.LEFT)
+        self.txt_carbs = ft.TextField(label="Carbs", value=str(self.carbs), on_change=self.update_values, text_align=ft.TextAlign.LEFT)
+        self.txt_protein = ft.TextField(label="Protein", value=str(self.protein), on_change=self.update_values, text_align=ft.TextAlign.LEFT)
+        self.txt_kcal = ft.TextField(label="Kcal", value=str(self.kcal), on_change=self.update_values, text_align=ft.TextAlign.LEFT)
 
         row_macros = ft.Row(controls=[self.txt_name, self.txt_fat, self.txt_carbs, self.txt_protein, self.txt_kcal])
 
         self.controls = [
             row_macros
         ]
+
+    def update_values(self, e):
+        self.name = self.txt_name.value
+        self.fat = float(self.txt_fat.value)
+        self.carbs = float(self.txt_carbs.value)
+        self.protein = float(self.txt_protein.value)
+        self.kcal = int(self.txt_kcal.value)
 
 
 class SimpleMacrosApp(ft.Column):
@@ -30,22 +37,15 @@ class SimpleMacrosApp(ft.Column):
 
         self.ingredients = ft.Column()
 
+        self.form_ingredient = Ingredient(name="NAMMMA", fat=0, carbs=0, protein=0, kcal=0)
 
-        self.txt_name = ft.TextField(label="Name", value="", text_align=ft.TextAlign.LEFT)
-        self.txt_fat = ft.TextField(label="Fat", value="0", text_align=ft.TextAlign.RIGHT)
-        self.txt_carbs = ft.TextField(label="Carbs", value="0", text_align=ft.TextAlign.RIGHT)
-        self.txt_protein = ft.TextField(label="Protein", value="0", text_align=ft.TextAlign.RIGHT)
-        self.txt_kcal = ft.TextField(label="Kcal", value="0", text_align=ft.TextAlign.RIGHT)
-
-
-
-        row_macros = ft.Row(controls=[self.txt_name, self.txt_fat, self.txt_carbs, self.txt_protein, self.txt_kcal])
-
-        self.txt_sum_of_macros = ft.TextField(label="Total", value="0", text_align=ft.TextAlign.JUSTIFY)
+        row_macros = ft.Row(controls=[self.form_ingredient])
 
         btn_add = ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=self.add_clicked)
         btn_calculate = ft.FloatingActionButton(icon=ft.Icons.CALCULATE, on_click=self.calculate_click)
 
+        self.txt_sum_of_macros = ft.TextField(label="Total", value="0", text_align=ft.TextAlign.JUSTIFY)
+        
         self.controls = [
             ft.Row(controls=[row_macros]),
             ft.Row(controls=[btn_add, btn_calculate]),
@@ -59,7 +59,12 @@ class SimpleMacrosApp(ft.Column):
         ]
 
     def add_clicked(self, e):
-        ingredient = Ingredient(self.txt_name.value, self.txt_fat.value, self.txt_carbs.value, self.txt_protein.value, self.txt_kcal.value)
+        # self.form_ingredient.update_value()
+        ingredient = Ingredient(name=self.form_ingredient.name,
+                                fat=self.form_ingredient.fat,
+                                carbs=self.form_ingredient.carbs,
+                                protein=self.form_ingredient.protein,
+                                kcal=self.form_ingredient.kcal)
         self.ingredients.controls.append(ingredient)
         self.update()
     
