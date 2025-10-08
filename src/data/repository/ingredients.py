7ea4c6310a2b7d265f51b84503ed0e307b2ecb09
toriium <John.Doe@hotmail.com>
2025-10-08 22:57:07 +0200

    feat: add AddIngredientsRow

--- src/data/repository/ingredients.py
+++ src/data/repository/ingredients.py
@@ -1,13 +1,18 @@
 from src.data.db_orm.query_obj import select_all_obj
 from src.data.db_orm.tables.tbl_ingredients import TblIngredients
-from src.data.db_orm.query_obj import delete_obj
+from src.data.db_orm.query_obj import delete_obj, insert_obj
 
 
 
 class IngredientsRepository:
     def get_all_ingredients(self) -> list[TblIngredients]:
         return select_all_obj(obj_table=TblIngredients)
-    
+
+    def add_ingredient(self, ingredient: TblIngredients) -> None:
+        _, err = insert_obj(obj=ingredient)
+        if err:
+            print(f"Error adding ingredient: {err}")
+
     def delete_ingredient(self, ingredient_id: int) -> None:
         err = delete_obj(obj_table=TblIngredients, where_clauses=[TblIngredients.id == ingredient_id])
         if err:
--- src/pages/ingredients.py
+++ src/pages/ingredients.py
@@ -5,7 +5,7 @@ from src.data.repository.ingredients import IngredientsRepository
 from src.utils.table_builder import TableBuilder
 
 
-class IngredientsPage(ft.Column):
+class IngredientsTable(ft.Column):
     def __init__(self):
         super().__init__()
         self.expand = True
@@ -15,10 +15,13 @@ class IngredientsPage(ft.Column):
         columns.append(ft.DataColumn(ft.Text("Actions")))
 
         self.data_table = ft.DataTable(columns=columns, rows=[])
-        self.update_table()
+        # self.update_table()
         
         text = ft.Text("Ingredients Page", size=30)
         self.controls = [text, self.data_table]
+    
+    def did_mount(self):
+        self.update_table()
 
     def update_table(self):
         ingredients = IngredientsRepository().get_all_ingredients()
@@ -35,9 +38,81 @@ class IngredientsPage(ft.Column):
             row_cells = [ft.DataCell(ft.Text(str(item))) for item in ingredient]
             row_cells.append(ft.DataCell(delete_btn))
             self.data_table.rows.append(ft.DataRow(cells=row_cells))
+        
+        self.update()
+
 
     def delete_ingredient(self, ingredient: TblIngredients):
         # IngredientsRepository().delete_ingredient(ingredient.id)
         IngredientsRepository().delete_ingredient(ingredient[0])
         self.update_table()
+
+
+
+class AddIngredientsRow(ft.Row):
+    def __init__(self, update_table_callback: callable):
+        super().__init__()
+        # self.expand = True
+
+        self.update_table_callback = update_table_callback
+
+        self.name = ft.TextField(label="Name", value="", text_align=ft.TextAlign.LEFT,width=100, dense=True)
+        self.quantity = ft.TextField(label="Quantity", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+        self.fat = ft.TextField(label="Fat", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+        self.carbs = ft.TextField(label="Carbs", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+        self.fiber = ft.TextField(label="Fiber", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+        self.protein = ft.TextField(label="Protein", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+        self.kcal = ft.TextField(label="Kcal", value=0, text_align=ft.TextAlign.LEFT, width=100, dense=True)
+
+        self.controls = [
+            self.name,
+            self.quantity,
+            self.fat,
+            self.carbs,
+            self.fiber,
+            self.protein,
+            self.kcal,
+            ft.ElevatedButton("Add Ingredient", on_click=self.add_ingredient)
+        ]
+    
+    def add_ingredient(self, e):
+        ingredient_obj = TblIngredients(
+            name=self.name.value,
+            quantity=float(self.quantity.value),
+            fat=float(self.fat.value),
+            carbohydrates=float(self.carbs.value),
+            fiber=float(self.fiber.value),
+            protein=float(self.protein.value),
+            kcal=float(self.kcal.value)
+        )
+        IngredientsRepository().add_ingredient(ingredient_obj)
+
+
+        # Clear fields after adding
+        self.name.value = ""
+        self.quantity.value = "0"
+        self.fat.value = "0"
+        self.carbs.value = "0"
+        self.fiber.value = "0"
+        self.protein.value = "0"
+        self.kcal.value = "0"
         self.update()
+
+        # Notify the parent to update the table
+        self.update_table_callback()
+
+class IngredientsPage(ft.Column):
+    def __init__(self):
+        super().__init__()
+        self.expand = True
+        
+        self.ingredients_table = IngredientsTable()
+        self.add_ingredients_row = AddIngredientsRow(update_table_callback=self.ingredients_table.update_table)
+
+        add_row_container = ft.Container(
+            content=self.add_ingredients_row,
+            height=60,  # ajuste conforme necessário
+            padding=1
+        )
+
+        self.controls = [add_row_container, self.ingredients_table]
