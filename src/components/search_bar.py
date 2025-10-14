5fedb917c1fe2cea46e458e791ff60a0b64be19d
toriium <you@example.com>
2025-10-14 14:49:18 +0200

    feat: add CustomSearchBar

---
+++ src/components/search_bar.py
@@ -0,0 +1,36 @@
+import flet as ft
+from abc import abstractmethod, ABC
+
+class CustomSearchBar(ft.SearchBar, ABC):
+    def __init__(self):
+        super().__init__()
+        self.view_elevation = 4
+        self.divider_color = ft.Colors.BLUE
+        self.bar_hint_text = "Search..."
+        self.view_hint_text = "Start typing to view options..."
+        self.on_change = self.handle_change
+        self.on_tap = self.open_anchor
+
+        self.lv = ft.ListView()
+        self.controls = [self.lv]
+    
+    @abstractmethod
+    def items_to_show(self, typed_text:str) -> list[str]:
+        ...
+
+    def handle_change(self, e: ft.ControlEvent):
+        items_to_show = self.items_to_show(e.data.lower())
+      
+        self.lv.controls.clear()
+        for i in items_to_show:
+            list_tile = ft.ListTile(
+                title=ft.Text(f"{i}"), on_click=self.close_anchor, data=i
+            )
+            self.lv.controls.append(list_tile)
+        self.update()
+
+    def open_anchor(self, e: ft.ControlEvent):
+        self.open_view()
+
+    def close_anchor(self, e: ft.ControlEvent):
+        self.close_view(e.control.data)
